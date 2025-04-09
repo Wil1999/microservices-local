4d95ab53243e45acd40b6ed93d4e4b7849941404
Wil1999 <wil1999_@hotmail.com>
2025-04-09 16:15:31 -0500

    agregando configuracion eureka-service

--- usuario-service/src/main/java/com/usuario_service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario_service/UsuarioServiceApplication.java
@@ -2,11 +2,13 @@ package com.usuario_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients(basePackages = "com.usuario_service.feign")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
